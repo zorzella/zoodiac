ca373a77375682c5b07a01b2c73d6a36a281b803
Tim De Baets <982167+tdebaets@users.noreply.github.com>
2025-05-17 17:45:19 +0200

    Refresh the project files and build the project in a separate WorkspaceJob object
    This makes Eclipse show the visual progress of the refresh/build as a job in the Progress tab in Eclipse's user interface.
    Also added some logging so that it's clear in Eclipse's log file what was happening and at what time.

--- plugin/src/com/google/zoodiac/refreshnow/RefreshNowTask.java
+++ plugin/src/com/google/zoodiac/refreshnow/RefreshNowTask.java
@@ -12,11 +12,11 @@
 package com.google.zoodiac.refreshnow;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -27,9 +27,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 class RefreshNowTask extends TimerTask {
 
-  private static final String REFRESH_NOW_COMPLETED_MESSAGE = "Refresh now completed";
-  private static final String REFRESH_NOW_IN_PROGRESS_MESSAGE = "Refresh now in progress";
-
   /**
    * Prevents some actions (such as shutdown) from happening at the very first 
    * time this is run. 
@@ -38,79 +35,65 @@ class RefreshNowTask extends TimerTask {
   
   @Override
   public synchronized void run() {
+    IWorkspaceRoot root;
+
     try {
-      IWorkspaceRoot root;
+      root = ResourcesPlugin.getWorkspace().getRoot();
+    } catch (IllegalStateException e) {
+      // The workspace has been closed (i.e. Eclipse is exiting). Ignoring it.
+      return;
+    }
 
-      try {
-        root = ResourcesPlugin.getWorkspace().getRoot();
-      } catch (IllegalStateException e) {
-        // The workspace has been closed (i.e. Eclipse is exiting). Ignoring it.
-        return;
-      }
+    final File shutdownNowWorkspace = 
+      new File (new File(root.getLocationURI()), "shutdownnow");
 
-      final File shutdownNowWorkspace = 
-        new File (new File(root.getLocationURI()), "shutdownnow");
-
-      final File refreshNowWorkspace = 
-        new File (new File(root.getLocationURI()), "refreshnow");
-      
-      boolean refreshAll = refreshNowWorkspace.delete();
-      
+    final File refreshNowWorkspace = 
+      new File (new File(root.getLocationURI()), "refreshnow");
+    
+    boolean refreshAll = refreshNowWorkspace.delete();
+    
+    if (shutdownNowWorkspace.exists()) {
       if (shutdownNowWorkspace.exists()) {
-        if (shutdownNowWorkspace.exists()) {
-          Thread deleteShutdownnowFile = new Thread(new Runnable() {
-            @Override
-            public void run() {
-              shutdownNowWorkspace.delete();
-            }
-          });
-          Runtime.getRuntime().addShutdownHook(deleteShutdownnowFile);
-          shutdownnow(shutdownNowWorkspace);
-        }
-      } else {
-        IProject[] projects = root.getProjects();
-        for (IProject project : projects) {
-          File uri = new File(project.getLocationURI());
-          File refreshNow = new File(uri, "refreshnow");
-          if (refreshAll || refreshNow.delete()) {
-            refreshnow(project);
-          } else {
-            final File shutdownNow = new File(uri, "shutdownnow");
-            if (shutdownNow.exists()) {
-              Thread deleteShutdownnowFile = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                  // We delete the "shutdownnow" file as a JVM exit hook, so
-                  // its absence can be used as an indicator that the shutdown
-                  // is complete
-                  shutdownNow.delete();
-                }
-              });
-              Runtime.getRuntime().addShutdownHook(deleteShutdownnowFile);
-              shutdownnow(shutdownNow);
-            }
+        Thread deleteShutdownnowFile = new Thread(new Runnable() {
+          @Override
+          public void run() {
+            shutdownNowWorkspace.delete();
+          }
+        });
+        Runtime.getRuntime().addShutdownHook(deleteShutdownnowFile);
+        shutdownnow(shutdownNowWorkspace);
+      }
+    } else {
+      IProject[] projects = root.getProjects();
+      for (IProject project : projects) {
+        File uri = new File(project.getLocationURI());
+        File refreshNow = new File(uri, "refreshnow");
+        if (refreshAll || refreshNow.delete()) {
+          refreshnow(project);
+        } else {
+          final File shutdownNow = new File(uri, "shutdownnow");
+          if (shutdownNow.exists()) {
+            Thread deleteShutdownnowFile = new Thread(new Runnable() {
+              @Override
+              public void run() {
+                // We delete the "shutdownnow" file as a JVM exit hook, so
+                // its absence can be used as an indicator that the shutdown
+                // is complete
+                shutdownNow.delete();
+              }
+            });
+            Runtime.getRuntime().addShutdownHook(deleteShutdownnowFile);
+            shutdownnow(shutdownNow);
           }
         }
       }
-      firstTime = false;
-    } catch (final CoreException e) {
-      String balloonTitle = "RefreshNow plugin";
-      String message = e.getMessage();
-      displayMessage(balloonTitle, message, false);
     }
+    firstTime = false;
   }
 
-  private void refreshnow(IProject project) throws CoreException {
-    String balloonTitle = String
-      .format("Project '%s'", project.getName());
-    displayMessage(balloonTitle, REFRESH_NOW_IN_PROGRESS_MESSAGE, true);
-    project.refreshLocal(IResource.DEPTH_INFINITE, null);
-
-    project.build(
-        IncrementalProjectBuilder.INCREMENTAL_BUILD,
-        null);
-    
-    displayMessage(balloonTitle, REFRESH_NOW_COMPLETED_MESSAGE, true);
+  private void refreshnow(IProject project) {
+    RefreshNowJob job = new RefreshNowJob(project);
+    job.schedule();
   }
 
   private AtomicBoolean shuttingDown = new AtomicBoolean();
@@ -129,16 +112,21 @@ class RefreshNowTask extends TimerTask {
     
       @Override
       public void run() {
+        logInfo("Shutting down now");
         IWorkbench workbench = PlatformUI.getWorkbench();
         workbench.saveAllEditors(false);
         workbench.close();
       }
     };
-    Display.getDefault().asyncExec(
-      shutdownRunnable);
+    Display.getDefault().asyncExec(shutdownRunnable);
+  }
+  
+  void logInfo(String message) {
+    ILog log = Platform.getLog(getClass());
+    log.log(new Status(Status.INFO, "com.google.zoodiac", message));
   }
 
-  private void displayMessage(String balloonTitle, String message,
+  static void displayMessage(String balloonTitle, String message,
       boolean autoHide) {
     Display.getDefault().asyncExec(
       new RefreshNowMessage(balloonTitle, message, autoHide));
